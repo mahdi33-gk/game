1c2b3d2b7424104c395943da6f0c1fc425d1adbc
gk.khan <meheduvau@gmail.com>
2024-10-06 21:32:36 +0600

    add html

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-///
+function display(){
+    const ul = document.getElementById('users-list');
+    for(const user of data){
+        console.log(user.name);
+        const li = document.createElement('li');
+        li.innerText ='user'
+    }
+}
